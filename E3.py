150db1daffca697bffc435539ad8d482a159d353
ginaser <sergina1493@yahoo.com>
2017-06-21 17:49:38 -0600

    Add files via upload

---
+++ E3.py
@@ -0,0 +1,25 @@
+import argparse
+
+if __name__ == '__main__':
+
+	parser=argparse.ArgumentParser()
+	parser.add_argument('nombre')
+	
+	args=parser.parse_args()
+
+	print(args.nombre)
+
+	estudiante = {
+	    'nombre': 'args.nombre',
+	    
+	}
+	print nombre
+	if dato in estudiante.iteritems():
+		{
+		print existe
+		}
+		else
+		{
+		if dato in estudiante.iteritems():
+		print no existe
+		}
