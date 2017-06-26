db62a504b18d26e8c322e9e70dcc44f38688e6dd
ginaser <sergina1493@yahoo.com>
2017-06-26 17:01:15 -0600

    Add files via upload

---
+++ P2D.py
@@ -0,0 +1,22 @@
+import argparse
+
+	estudiante = {
+	     'nombre': args.nombre,
+	}
+  
+	if __name__ == '__main__':
+
+	parser=argparse.ArgumentParser()
+	parser.add_argument('nombre')
+	args=parser.parse_args()
+
+	print(args.nombre)
+	
+	if nombre, args.nombre in estudiante.iteritems():
+		print true
+			else:
+				print false
+
+
+
+		
