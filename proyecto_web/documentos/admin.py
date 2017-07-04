a47a1021c96917dc3a8d461cf13e22c992737045
sergina <sergina1493@yahoo.com>
2017-07-04 17:59:16 -0600

    web clase 2

--- proyecto_web/documentos/admin.py
+++ proyecto_web/documentos/admin.py
@@ -4,3 +4,14 @@ from __future__ import unicode_literals
 from django.contrib import admin
 
 # Register your models here.
+
+# importar modelo
+from documentos.models import Documento
+
+#crear subclase de admin.ModelAdmin
+class DocumentoAdmin(admin.ModelAdmin):
+	model = Documento
+
+#Registrar MOdelAdmin para cada modelo
+
+admin.site.register(Documento,DocumentoAdmin)
