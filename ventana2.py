5eb3ab148136d1f31f354c7a758288bc7bd23705
ginaser <sergina1493@yahoo.com>
2017-07-10 17:54:44 -0600

    Add files via upload

--- ventana2.py
+++ ventana2.py
@@ -1,11 +1,19 @@
+import gi
+
+gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk
+
+def imprimir_algo(btn):
+	print btn
+	print 'HOLA MUNDO'
 
 if __name__ == '__main__':
 
-	ventana = Gtk.Window(title='Ejemplo3')
+	ventana = Gtk.Window(title='Ejemplo2')
 	ventana.connect('delete-event' , Gtk.main_quit)
 	boton = Gtk.Button('Btn 1')
-	
+	#boton.connect('clicked',imprimir_algo)
+	boton2 = Gtk.Button('Boton 2')
 
 	contenedor = Gtk.Grid()
 	contenedor.set_column_homogeneous(True)
@@ -13,12 +21,12 @@ if __name__ == '__main__':
 
 	contenedor.attach(
 	boton , # Elemento
-	1, #columna
-	3, #fila
-	2, #Nro columnas a usar
-	2, #Nro Filas a usar
+	0, #columna
+	0, #fila
+	1, #Nro columnas a usar
+	1, #Nro Filas a usar
 )
-	contenedor.attach(boton, 1, 2, 1, 0)
+	contenedor.attach(boton2, 0, 1, 1, 1)
 	ventana.add(contenedor)
 	ventana.show_all()
 	Gtk.main()
