557d9a18a54e01e471732177c54e5cfb0ec8114a
ginaser <sergina1493@yahoo.com>
2017-07-11 17:14:27 -0600

    Add files via upload

--- python-gtk/ventana3.py
+++ python-gtk/ventana3.py
@@ -4,24 +4,37 @@ gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk
 
 class MiVentana(Gtk.Window):
-	def _init_(self, *args, **kwargs):
-		super(MiVentana, self)._init_(*args, **kwargs)
+	def __init__(self, *args, **kwargs):
+		super(MiVentana, self).__init__(*args, **kwargs)
 		self.set_default_size(500,300)
 		self.connect('delete-event', Gtk.main_quit)
+		self.agregar_contenedor()
+		self.agregar_boton()
+		self.agregar_texto()
+		self.agregar_etiqueta()
 
+		
 	def agregar_contenedor(self):
 		self.contenedor = Gtk.Grid()
+	
+	def agregar_texto(self):
+		entrada = Gtk.Entry()
+		self.contenedor.attach(
+			entrada,
+			0,
+			0,
+			3,
+			1
+		)
 
-	def agregarboton(MiVentana):
-		boton2 = Gtk.Button('Boton 2')
-		ventana = Gtk.Window(title='Ejemplo3')
+	def agregar_boton(self):
+		Ventana = Gtk.Window(title = 'MENSAJE')
+		boton = Gtk.Button('Boton 1')
+		
 
-		boton.connect = ('Clicked' , imprimir_algo)
-		boton2 = Gtk.Button('Boton 2')
-	
-		contenedor = Gtk.VBox()
-	
-	
+	def agregar_etiqueta(self):
+		etiqueta = Gtk.Label()
+		
 
 if __name__ == '__main__':
 	ventana = MiVentana()
