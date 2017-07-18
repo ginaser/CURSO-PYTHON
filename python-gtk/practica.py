61f105804cfc33d726de870f42cad84569c2048b
Udem <udem@fedora.mshome.net>
2017-07-18 16:36:31 -0600

    ultima modificaciones

---
+++ python-gtk/practica.py
@@ -0,0 +1,25 @@
+import gi
+
+gi.require_version('Gtk', '3.0')
+
+from gi.repository import Gtk, Gio, GLib
+
+from PASIVOACTIVO import MiVentana
+
+class CursoPythonApp(Gtk.Application):
+
+	def __init__(self, *args, **kwargs):
+
+		super(CursoPythonApp, self).__init__(*args,application_id='ni.edu.udem.cursopython.ss',** kwargs)
+		self.window = None
+
+	def  do_activate(self):
+
+		if not self.window:
+			self.window = MiVentana(application = self, title ='Ventana Principal')
+			self.window.show_all()
+			self.window.present()
+
+if __name__ == '__main__':
+	app=CursoPythonApp()
+	app.run()
